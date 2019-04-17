19cdb29ad21d28643033f17f1028a18f3cffc643
André Luis Leal Cardoso Junior <andrehjr@gmail.com>
2019-04-17 13:10:10 -0300

    bump path test

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use std::path::Path;
+
+fn main() {
+    if let Some(root) = Path::new("/foo/bar").components().next() {
+        let meh = root.as_os_str().to_str().unwrap();
+        println!("{:?}", "/foo/bar".trim_start_matches(meh));
+    }
+
+    println!("{:?}", Path::new("/foo").components());
+    println!("{:?}", Path::new("/foo/bar").components().map(|element| element.as_os_str() ).collect::<Vec<_>>());
+
+
+    println!("{:?}", Path::new("//foo").components());
+    println!("{:?}", Path::new("//foo/bar").components().map(|element| element.as_os_str() ).collect::<Vec<_>>());
+
+    println!("{:?}", Path::new("C:/foo").components());
+    println!("{:?}", Path::new("C://foo/bar").components().map(|element| element.as_os_str() ).collect::<Vec<_>>());
+
+}
